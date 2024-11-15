d54edcb3d9153427f1ded4dd0c13c4d8d4ce9f59
sourabh07032000 <113441171+sourabh07032000@users.noreply.github.com>
2024-11-15 17:07:38 +0530

    Update Feedback.js

--- models/Feedback.js
+++ models/Feedback.js
@@ -1,1 +1,12 @@
+const mongoose = require('mongoose');
 
+// Plan schema
+const FeedbackSchema = new mongoose.Schema({
+  name : String,
+  email : String,
+  phoneNumber : Number,
+  message : String,
+
+});
+
+module.exports = mongoose.model('Feedback', FeedbackSchema);
